3fc61261a3d7dec930a43b97d134b873a336dca3
lotostoii <lotostoii@gmail.com>
2021-03-09 10:33:58 +1000

    grey theme

--- public/src/js/resume.js
+++ public/src/js/resume.js
@@ -1,58 +1,60 @@
 class DropWindow {
-    constructor(options) {
-        this.$el = typeof options.contSel === 'string' ? $.el(options.contSel) : options.contSel
-        this.$button = $.el(options.switchSel, this.$el)
-        this.$indicator = $.el(options.indicator, this.$el)
-        this.$content = $.el(options.contentSel, this.$el)
-        this.height = 0
-        this._handler()
-        this._getHeight()
-    }
+  constructor(options) {
+    this.$el = typeof options.contSel === 'string' ? $.el(options.contSel) : options.contSel
+    this.$button = $.el(options.switchSel, this.$el)
+    this.$indicator = $.el(options.indicator, this.$el)
+    this.$content = $.el(options.contentSel, this.$el)
+    this.height = 0
+    this._handler()
+    this._getHeight()
+  }
 
-    async _getHeight() {
-        let { $content, $indicator } = this
-        if (!$content.classList.contains('active')) {
-            $content.classList.add('measuring')
-            await _await(100)
-            this.height = $content.offsetHeight
-            await _await(100)
-            $content.classList.remove('measuring')
-            await _await(100)
-        } else {
-            await _await(300)
-            this.height = $content.offsetHeight
-            $content.style.height = `${this.height}px`
-            $indicator && $indicator.classList.add('active')
-        }
-        $content.style.transition = 'margin 300ms,padding 300ms, height 300ms, opacity 300ms'
+  async _getHeight() {
+    let { $content, $indicator } = this
+    if (!$content.classList.contains('active')) {
+      $content.classList.add('measuring')
+      await _await(100)
+      this.height = $content.offsetHeight
+      console.log(this.height, 222)
+      await _await(100)
+      $content.classList.remove('measuring')
+      await _await(100)
+    } else {
+     // await _await(300)
+      $content.style.height = `auto`
+      $indicator && $indicator.classList.add('active')
+      await _await(100)
+      this.height = $content.offsetHeight
     }
+    $content.style.transition = 'margin 300ms,padding 300ms, height 300ms, opacity 300ms'
+  }
 
-    async _switchWindow(e) {
-        let { $content, $indicator } = this
-        if (!$content.classList.contains('active')) {
-            $content.classList.add('active')
-            $indicator && $indicator.classList.add('active')
-            $content.style.height = `${this.height}px`
-        } else {
-            $indicator && $indicator.classList.remove('active')
-            $content.classList.remove('active')
-            $content.style.height = `${0}px`
-        }
+  async _switchWindow(e) {
+    let { $content, $indicator } = this
+    if (!$content.classList.contains('active')) {
+      $content.classList.add('active')
+      $indicator && $indicator.classList.add('active')
+      $content.style.height = `${this.height}px`
+    } else {
+      $indicator && $indicator.classList.remove('active')
+      $content.classList.remove('active')
+      $content.style.height = `${0}px`
     }
-    _handler() {
-        this.$button.addEventListener('click', this._switchWindow.bind(this))
-    }
+  }
+  _handler() {
+    this.$button.addEventListener('click', this._switchWindow.bind(this))
+  }
 }
 
 const blokes = [...$.all('.resume__allInformation')]
 
 blokes.forEach(async (b) => {
-    const bloke = {
-        contSel: b,
-        switchSel: '.title',
-        contentSel: '.content',
-        indicator: 'i',
-        animTime: 700,
-    }
-    new DropWindow(bloke)
+  const bloke = {
+    contSel: b,
+    switchSel: '.title',
+    contentSel: '.content',
+    indicator: 'i',
+    animTime: 700,
+  }
+  new DropWindow(bloke)
 })
